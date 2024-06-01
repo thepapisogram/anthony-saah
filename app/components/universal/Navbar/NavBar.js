c9a16c512e020f80a3eb8df0adc6aacabc0f41e8
Anthony Saah <thepapisogram@gmail.com>
2024-06-01 12:12:59 +0000

    logo updated; site_info updated; /apps created

--- app/components/universal/Navbar/NavBar.js
+++ app/components/universal/Navbar/NavBar.js
@@ -4,9 +4,8 @@ import Logo from "@/app/components/universal/Logo"
 export default function NavBar() {
 
   return (
-    <div className="flex items-center py-2">
-      <Logo  width={50} height={50} />
-      <h1 className="dark:text-yellow-500 font-bold tracking-widest text-xl ml-1">APISO</h1>
+    <div className="flex items-center py-4">
+      <Logo />
       <NavIcons />
     </div>
   )
