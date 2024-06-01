bd1b218ee6129162270f40deaa79830ff68b87ca
Anthony Saah <thepapisogram@gmail.com>
2024-06-01 03:02:17 +0000

    footer margin updated

--- app/components/universal/Footer/Footer.js
+++ app/components/universal/Footer/Footer.js
@@ -1,8 +1,8 @@
 export default function Footer({ author, year, last_updated }) {
   return (
     <>
-      <hr className="bg-neutral-900 dark:bg-neutral-700 bg-opacity-50 p-0.5 border-none rounded-full mt-5" />
-      <footer className="text-neutral-600 text-sm font-mono my-5">
+      <hr className="bg-neutral-900 dark:bg-neutral-700 bg-opacity-50 p-0.5 border-none rounded-full my-5" />
+      <footer className="text-neutral-600 text-sm font-mono">
         <p>&copy; Copyright {author}</p>
         <pre>  All rights reserved</pre>
         <pre>  Proudly 🇬🇭</pre>
