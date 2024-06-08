d6d7491ebac02f7130639c8b42a3becbae79f058
Anthony Saah <thepapisogram@gmail.com>
2024-06-08 02:08:03 +0000

    Calcutor theming complete

--- app/projects/calculator/page.js
+++ app/projects/calculator/page.js
@@ -9,6 +9,7 @@ export default function page() {
 
   const [screen, setScreen] = useState('');
   const [lastSymbol, setLastSymbol] = useState('');
+  const [bracketOpen, setBracketOpen] = useState(false);
 
   // Backspace & Clear
   const backSpace = () => {
@@ -17,6 +18,7 @@ export default function page() {
   const clear = () => {
     setLastSymbol('')
     setScreen('')
+    setBracketOpen(false);
   }
 
   // 0 - 9
@@ -26,13 +28,13 @@ export default function page() {
   // .
   const insPoint = () => {
     if (screen == '') setScreen('0.');
-    else if (lastSymbol == "." && screen.endsWith('.')) console.log('err: . is last symbol');
+    else if (screen.endsWith('(')) setScreen(`${screen}0.`)
+    else if (lastSymbol == ".") console.log('err: . is last symbol');
     else if (lastSymbol == '.') console.log('err: . is the last symbol');
     else if (lastSymbol == "+" || lastSymbol == "-" || lastSymbol == "/" || lastSymbol == "*"){
       if (!screen.endsWith('.')) setScreen(`${screen}.`);
       else setScreen(`${screen}0.`)
     }
-    else if (lastSymbol == "." && !screen.endsWith('.')) setScreen(`${screen}0.`);
     else if (lastSymbol == '') setScreen(`${screen}.`);
     else setScreen(`${screen}0.`)
 
@@ -52,26 +54,62 @@ export default function page() {
     setLastSymbol(op)
   }
 
+  // ( )
+  const bracketHandler = (bracket) => {
+    if(bracket == '('){
+      if(bracketOpen === true){
+        if(screen.endsWith(bracket)) console.log(`err: ending with ${bracket}`)
+        else if(screen.endsWith('.')) console.log('err: ending with a .')
+        else if (screen.endsWith("+") || screen.endsWith("-") || screen.endsWith("/") || screen.endsWith("*")) console.log('err. ending with operand')
+        else console.log('( is still open')
+      } else{
+        if(screen === '') setScreen('(');
+        else if (lastSymbol === '') setScreen(`${screen}*(`)
+        else if (screen.endsWith("+") || screen.endsWith("-") || screen.endsWith("/") || screen.endsWith("*")){
+          setScreen(`${screen}(`)
+        }
+        else if (screen.endsWith('.')) console.log('err: ending with .')
+        else setScreen(`${screen}*(`);
+        setBracketOpen(true);
+      }
+      setBracketOpen(true)
+    } else{
+      if (bracketOpen === true) {
+        if(screen === '') console.log('err: no open bracket');
+        else if (screen.endsWith(bracket)) console.log(`err: ending with ${bracket}`)
+        else if (screen.endsWith('(')) setScreen(`${screen}1)`)
+        else setScreen(`${screen})`)
+        setBracketOpen(false)
+      }
+    }
+  }
+
   // =
   const insAns = () => {
-    const answer = eval(screen).toFixed(1);
-    if(answer.endsWith('.0')) setScreen(`${answer.slice(0, -2)}`);
+    let answer = eval(screen).toFixed(1);
+    if(answer.endsWith('.0')){
+      setScreen(`${answer.slice(0, -2)}`);
+      answer = answer.slice(0, -2);
+    }
     else setScreen(`${answer}`);
 
     if (answer.includes('.')) setLastSymbol('.');
     else setLastSymbol('');
+
+    setBracketOpen(false);
   }
 
   return (
     <div className="p-2 my-3">
       <BackLink link="/projects" text="Projects" />
-      <div className="bg-stone-800 w-full md:w-80 mx-auto rounded-lg p-3">
-        <h1 className="text-center head-text tracking-wide font-thin text-xl">Simple Calculator</h1>
+      <div className="calculator">
+        <h1 className="calc-text head-text">Simple Calculator</h1>
         <input type="text" className="calc-input" placeholder="0" value={screen} readOnly />
         <div className="grid grid-cols-4 gap-2">
-          <button className="edge-btn">(</button>
-          <button className="edge-btn">)</button>
-          <button className="edge-btn" onClick={backSpace}>&#x232B;</button>
+          <button className="edge-btn" onClick={clear}>CLS</button>
+          <button className="edge-btn" onClick={(e) => bracketHandler(e.target.innerText)}>(</button>
+          <button className="edge-btn" onClick={(e) => bracketHandler(e.target.innerText)}>)</button>
+          {/* <button className="edge-btn" onClick={backSpace}>&#x232B;</button> */}
           <button className="edge-btn" onClick={() => insOp('/')}>&divide;</button>
           <button className="reg-btn" onClick={(e) => insNum(e.target.innerText)}>7</button>
           <button className="reg-btn" onClick={(e) => insNum(e.target.innerText)}>8</button>
@@ -80,14 +118,14 @@ export default function page() {
           <button className="reg-btn" onClick={(e) => insNum(e.target.innerText)}>4</button>
           <button className="reg-btn" onClick={(e) => insNum(e.target.innerText)}>5</button>
           <button className="reg-btn" onClick={(e) => insNum(e.target.innerText)}>6</button>
-          <button className="edge-btn" onClick={ (e) => insOp(e.target.innerText)}>&minus;</button>
+          <button className="edge-btn" onClick={ (e) => insOp('-')}>&minus;</button>
           <button className="reg-btn" onClick={(e) => insNum(e.target.innerText)}>1</button>
           <button className="reg-btn" onClick={(e) => insNum(e.target.innerText)}>2</button>
           <button className="reg-btn" onClick={(e) => insNum(e.target.innerText)}>3</button>
           <button className="edge-btn" onClick={ (e) => insOp(e.target.innerText)}>+</button>
           <button className="reg-btn" onClick={() => insPoint()}>&#46;</button>
           <button className="reg-btn" onClick={(e) => insNum(e.target.innerText)}>0</button>
-          <button className="reg-btn" onClick={clear}>C</button>
+          <button className="reg-btn">ANS</button>
           <button className="edge-btn" onClick={insAns}>&#61;</button>
         </div>
       </div>
