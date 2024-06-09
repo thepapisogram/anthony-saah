5a162971ba7601bd7f80e95ec74180e165c66514
Anthony Saah <thepapisogram@gmail.com>
2024-06-09 07:44:43 +0000

    minor changes

--- app/components/Projects/ProjectLink.js
+++ app/components/Projects/ProjectLink.js
@@ -3,8 +3,8 @@ import Link from "next/link"
 export default function ProjectLink({title, link, body}) {
   return (
     <Link href={link} className="project-link p-2 rounded-lg my-2 cursor-pointer">
-        <h1 className="main-text text-lg">{title}</h1>
-        <p className="sub-text leading-5">{body}</p>
+        <h1 className="head-text text-lg">{title}</h1>
+        <p className="main-text text-sm leading-5">{body}</p>
     </Link>
   )
 }
