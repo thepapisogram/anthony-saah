7db193287e414bd735c3bb22eee4996525076027
Anthony Saah <thepapisogram@gmail.com>
2024-06-10 19:58:51 +0000

    updated /projects

--- app/projects/page.js
+++ app/projects/page.js
@@ -8,7 +8,7 @@ export default function page() {
       <h1 className="text-xl main-text uppercase mb-2">Projects</h1>
       <p className="sub-text">After losing lots of code, I&apos;m starting over with my projects. Explore the new web applications I&apos;m building! From simple projects to large scale apps. Below are the projects I&apos;ve completed.</p>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 mt-5 gap-2">
-        <ProjectLink link="/projects/calculator" title="Calculator" body="One of the traditions. Of course I built a simple Calculator" />
+        <ProjectLink link="/projects/calculator" title="Calculator" body="A traditional JS Project. Of course I built a simple Calculator" />
       </div>
     </div>
   )
