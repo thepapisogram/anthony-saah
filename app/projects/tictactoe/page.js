e63970b0d1740888bd6d411387c58b7471cfc3ee
Anthony Saah <thepapisogram@gmail.com>
2024-06-11 23:44:16 +0000

    Started tictactoe algorithm

--- app/projects/tictactoe/page.js
+++ app/projects/tictactoe/page.js
@@ -3,13 +3,28 @@
 
 import '@/app/styles/projects/tictactoe.css'
 import BackLink from '@/app/components/universal/BackLink';
+import { useState } from 'react';
 
 export default function page() {
+
+  const [playerTurn, setPlayerTurn] = useState(true)
+
+  const [row1, setRow1] = useState([0, 0, 0])
+  const [row2, setRow2] = useState([0, 0, 0])
+  const [row3, setRow3] = useState([0, 0, 0])
+
+  const handleMove = (row, pos) => {
+    const row_name = `row${row}`;
+    let new_row = [...this[row_name]];
+
+    // new_row[pos] = 
+  }
 
   return (
     <div className="p-2 my-3">
       <BackLink link="/projects" text="Projects" />
-      <div className="tictactoe head-text">
+      <div className="tictactoe">
+        <h1 className='game-text'>Tic Tac Toe</h1>
         <div className="box">X</div>
         <div className="box">X</div>
         <div className="selected-box">X</div>
@@ -19,6 +34,7 @@ export default function page() {
         <div className="box">X</div>
         <div className="box">X</div>
         <div className="box">X</div>
+        <h1 className='game-text'>Computer Wins!</h1>
       </div>
     </div>
   )
