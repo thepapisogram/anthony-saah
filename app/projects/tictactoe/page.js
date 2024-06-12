41a12fb11ddd7f4219f02f058a18148852e004f7
Anthony Saah <thepapisogram@gmail.com>
2024-06-12 20:25:53 +0000

    tictactoe finished

--- app/projects/tictactoe/page.js
+++ app/projects/tictactoe/page.js
@@ -1,82 +1,116 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
+import { useState } from 'react';
 
 import '@/app/styles/projects/tictactoe.css'
 import BackLink from '@/app/components/universal/BackLink';
-import { useState } from 'react';
 
 export default function page() {
 
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
+  // State to hold if game is ongoing
+  const [ongoing, setOngoing] = useState(true);
 
-  const calculateWinner = (squares) => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++){
-      const [a, b, c] = lines[i];
-      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-        return squares[a];
+  // State to check current player (X or O)
+  const [playerTurn, setPlayerTurn] = useState(true)
+
+  // Game Status (Turn, Win, Draw)
+  const [status, setStatus] = useState("X's Turn")
+
+  // Game Squares
+  const [squares, setSquares] = useState(Array(9).fill(''))
+
+  // Announce Game End
+  const announceGame = (outcome, winner = '') => {
+
+    switch(outcome){
+      case 'win':
+        setStatus(`${winner} wins this game`);
+        break;
+      case 'draw':
+        setStatus('The game was tied');
+        break;
+    }
+
+    setOngoing(false)
+  }
+
+  // Check Win
+  const checkWin = () => {
+
+    // Check Horizontally
+    if(squares[0] !== '' && squares[0] === squares[1] && squares[1] === squares[2]){
+      announceGame('win', squares[0]);
+    } else if (squares[3] !== '' && squares[3] === squares[4] && squares[4] === squares[5]) {
+      announceGame('win', squares[3]);
+    } else if (squares[6] !== '' && squares[6] === squares[7] && squares[7] === squares[8]) {
+      announceGame('win', squares[6]);
+    } 
+    
+    // Check Vertically
+    else if (squares[0] !== '' && squares[0] === squares[3] && squares[3] === squares[6]) {
+      announceGame('win', squares[0]);
+    }  else if (squares[1] !== '' && squares[1] === squares[4] && squares[4] === squares[7]) {
+      announceGame('win', squares[1]);
+    } else if (squares[2] !== '' && squares[2] === squares[5] && squares[5] === squares[8]) {
+      announceGame('win', squares[2]);
+    }
+
+    // Check Diagonally
+    else if (squares[0] !== '' && squares[0] === squares[4] && squares[4] === squares[8]) {
+      announceGame('win', squares[0]);
+    } else if (squares[2] !== '' && squares[2] === squares[4] && squares[4] === squares[6]) {
+      announceGame('win', squares[2]);
+    }
+
+    // Check if all squares are filled. Then draw
+    else {
+      let allFilled = squares.every(square => square !== '');
+      if(allFilled){
+        announceGame('draw')
       }
     }
-    return null;
+
   }
 
-  const handleClick = (index) => {
-    if(calculateWinner(board) || board[index]){
-      return;
+  // When square is clicked. Check if its empty and render X or O based on playerTurn
+  const handleClick = (e, pos) => {
+    // Copy squares state so we can replace the clicked position
+    const newSquares = squares;
+    const squarePlayed = e.target.innerText;
+    if (squarePlayed === ''){
+      e.target.innerText = ( playerTurn ? "X" : "O");
+      newSquares[pos] = playerTurn ? "X" : "O";
+      setSquares(newSquares);
+      setStatus(playerTurn ? "O's turn" : "X's Turn")
+      setPlayerTurn(!playerTurn);
     }
-    const newBoard = board.slice();
-    newBoard[index] = xIsNext ? 'X' : 'O';
-    setBoard(newBoard)
-    setXIsNext(!xIsNext);
+    checkWin();
   }
 
-  const winner = calculateWinner(board);
-  const status = winner ? 'Winner: ' + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O');
-
-  const renderSquare = (index) => {
-    return(
-      <div className="square" onClick={() => handleClick(index)}>
-        {board[index]}
-      </div>
-    )
+  // Restart Game - Reset all state to default values
+  const restart = () => {
+    setOngoing(true);
+    setPlayerTurn(true)
+    setStatus("X's Turn")
+    setSquares(Array(9).fill(''))
   }
-
-  // const [playerTurn, setPlayerTurn] = useState(true)
-
-  // const [row1, setRow1] = useState([0, 0, 0])
-  // const [row2, setRow2] = useState([0, 0, 0])
-  // const [row3, setRow3] = useState([0, 0, 0])
-
-  // const handleMove = (row, pos) => {
-  //   const row_name = `row${row}`;
-  //   let new_row = [...this[row_name]];
-  // }
 
   return (
     <div className="p-2 my-3">
       <BackLink link="/projects" text="Projects" />
       <div className="tictactoe">
         <h1 className='game-info'>Tic Tac Toe</h1>
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
+        <button className="square" onClick={(e) => handleClick(e, 0)} disabled={!ongoing}>{squares[0]}</button>
+        <button className="square" onClick={(e) => handleClick(e, 1)} disabled={!ongoing}>{squares[1]}</button>
+        <button className="square" onClick={(e) => handleClick(e, 2)} disabled={!ongoing}>{squares[2]}</button>
+        <button className="square" onClick={(e) => handleClick(e, 3)} disabled={!ongoing}>{squares[3]}</button>
+        <button className="square" onClick={(e) => handleClick(e, 4)} disabled={!ongoing}>{squares[4]}</button>
+        <button className="square" onClick={(e) => handleClick(e, 5)} disabled={!ongoing}>{squares[5]}</button>
+        <button className="square" onClick={(e) => handleClick(e, 6)} disabled={!ongoing}>{squares[6]}</button>
+        <button className="square" onClick={(e) => handleClick(e, 7)} disabled={!ongoing}>{squares[7]}</button>
+        <button className="square" onClick={(e) => handleClick(e, 8)} disabled={!ongoing}>{squares[8]}</button>
         <h1 className='game-info'>{status}</h1>
+        {!ongoing && <button className='start-btn' onClick={restart}>Restart Game</button>}
       </div>
     </div>
   )
