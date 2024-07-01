a4e2e1837a43e55140b1215ce92d241291e1efc3
Anthony Saah <thepapisogram@gmail.com>
2024-07-01 14:34:26 +0000

    fixed the links on the homepage

--- app/components/Projects/HomeProjectList.js
+++ app/components/Projects/HomeProjectList.js
@@ -3,8 +3,8 @@ import ProjectLink from "./ProjectLink"
 export default function HomeProjectList() {
   return (
       <div className="md:grid md:grid-cols-2 md:gap-x-2 mb-0.5">
-          <ProjectLink title="Tic Tac Toe" body="Simple Tic Tac Toe Game" />
-          <ProjectLink title="Calculator" body="A basic calculator" />
+          <ProjectLink title="Tic Tac Toe" link="/projects/tictactoe" body="Simple Tic Tac Toe Game" />
+          <ProjectLink title="Calculator" link="/projects/calculator" body="A basic calculator" />
       </div>
   )
 }
